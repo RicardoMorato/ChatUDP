453a2d3455984da22a27ca553dbfd2e890a61483
Ricardo Morato <rmr@cin.ufpe.br>
2024-06-08 18:12:40 -0300

    feat: open correct path when parsing file into message stream

---
+++ message_serializer/serializer.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from message_serializer.constants import CHUNK_SIZE
+
+
+class MessageSerializer:
+    def __init__(self, chunk_size: Optional[int] = CHUNK_SIZE) -> None:
+        self.chunk_size = chunk_size
+
+    def parse_file_into_message_stream(self, file_path: str = ""):
+        with open(file_path, "r", encoding="utf-8") as file:
+            pass
