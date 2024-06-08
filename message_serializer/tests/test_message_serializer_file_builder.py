0c9b98753bd7b7097360586ea9d8cf3a56c1abb3
Ricardo Morato <rmr@cin.ufpe.br>
2024-06-08 20:07:52 -0300

    test: creates file with correct file path

---
+++ message_serializer/tests/test_message_serializer_file_builder.py
@@ -0,0 +1,16 @@
+from message_serializer.serializer import MessageSerializer
+
+DUMMY_FILE_NAME = "dummy-file.txt"
+
+
+class TestSerializerFileBuilder:
+    def test_creates_file_with_correct_file_path(self, mocker):
+        open_mock = mocker.patch("builtins.open")
+
+        message_serializer = MessageSerializer()
+        message_serializer.build_messages_file(file_name=DUMMY_FILE_NAME)
+
+        open_mock.assert_called_once_with(
+            f"/message_serializer/files/{DUMMY_FILE_NAME}", "a"
+        )
+
