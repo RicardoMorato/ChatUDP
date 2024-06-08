feb1d11ec30f47e9ba0c108a5344539ed9a179db
Ricardo Morato <rmr@cin.ufpe.br>
2024-06-08 20:07:24 -0300

    feat: creates file with correct file path

--- message_serializer/serializer.py
+++ message_serializer/serializer.py
@@ -1,3 +1,4 @@
+import os
 from typing import Optional
 from message_serializer.constants import CHUNK_SIZE
 
@@ -5,6 +6,7 @@ from message_serializer.constants import CHUNK_SIZE
 class MessageSerializer:
     def __init__(self, chunk_size: Optional[int] = CHUNK_SIZE) -> None:
         self.chunk_size = chunk_size
+        self.files_dir = "/message_serializer/files/"
 
     def parse_file_into_message_stream(self, file_path: str = ""):
         with open(file_path, "rb") as file:
@@ -17,3 +19,9 @@ class MessageSerializer:
                 text = content.decode(encoding="utf-8")
 
                 yield text
+
+    def build_messages_file(self, file_name: str = "", message: Optional[str] = None):
+        file_path = os.path.join(self.files_dir, file_name)
+
+        with open(file_path, "a") as file:
+            pass
