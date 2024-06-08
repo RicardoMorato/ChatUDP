07b6cc114ce0242dd70d0d85c6caa25c4f142957
Ricardo Morato <rmr@cin.ufpe.br>
2024-06-08 20:19:18 -0300

    test: adds message to file

--- message_serializer/tests/test_message_serializer_file_builder.py
+++ message_serializer/tests/test_message_serializer_file_builder.py
@@ -1,9 +1,20 @@
+import os
+import pytest
+
 from message_serializer.serializer import MessageSerializer
 
 DUMMY_FILE_NAME = "dummy-file.txt"
 
 
 class TestSerializerFileBuilder:
+    @pytest.fixture(autouse=True)
+    def remove_dummy_file(self):
+        yield
+        file_path = f"message_serializer/files/{DUMMY_FILE_NAME}"
+
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
     def test_creates_file_with_correct_file_path(self, mocker):
         open_mock = mocker.patch("builtins.open")
 
@@ -14,3 +25,17 @@ class TestSerializerFileBuilder:
             f"message_serializer/files/{DUMMY_FILE_NAME}", "a"
         )
 
+    def test_adds_message_to_file(self):
+        example_message = "MENSAGEM DE EXEMPLO"
+
+        message_serializer = MessageSerializer()
+        file_path = message_serializer.build_messages_file(
+            file_name=DUMMY_FILE_NAME, message=example_message
+        )
+
+        assert file_path is not None
+
+        with open(file_path, "r", encoding="utf-8") as test_file:
+            content = test_file.read()
+
+            assert content == example_message
