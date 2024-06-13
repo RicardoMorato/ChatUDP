f2a29d38545241151c73002ca2109dd5dc24e489
Ricardo Morato <rmr@cin.ufpe.br>
2024-06-13 20:19:24 -0300

    feat: use message serializer on client

--- client/client.py
+++ client/client.py
@@ -6,8 +6,13 @@ from common.constants import (
     SERVER_HOST,
     SERVER_PORT,
 )
+from message_serializer.serializer import MessageSerializer
 
 client_socket = socket(AF_INET, SOCK_DGRAM)
+
+message_serializer = MessageSerializer(chunk_size=MESSAGE_CHUNK_SIZE)
+
+tmp_file_name = "tmp_file_client"
 
 while True:
     message = input("Input message: ")
@@ -15,7 +20,16 @@ while True:
     if message == CLOSE_CLIENT_SOCKET_MESSAGE:
         break
 
-    client_socket.sendto(message.encode(), (SERVER_HOST, SERVER_PORT))
+    file_path = message_serializer.build_messages_file(
+        file_name=tmp_file_name, message=message
+    )
+
+    chunked_message = message_serializer.parse_file_into_message_stream(file_path)
+
+    for chunk in chunked_message:
+        client_socket.sendto(chunk.encode(), (SERVER_HOST, SERVER_PORT))
+
+    message_serializer.remove_file(tmp_file_name)
 
     received_message, server_address = client_socket.recvfrom(MESSAGE_CHUNK_SIZE)
 
