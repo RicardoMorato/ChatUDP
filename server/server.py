f0cf65d444c9dcc2e30d0e367dbd48194033d7b3
Ricardo Morato <rmr@cin.ufpe.br>
2024-06-13 19:55:51 -0300

    feat: create rudimentary server

---
+++ server/server.py
@@ -0,0 +1,28 @@
+from socket import *
+
+from common.constants import FINISH_SERVER_MESSAGE, MESSAGE_CHUNK_SIZE, SERVER_PORT
+
+server_port = 12000
+
+server_socket = socket(AF_INET, SOCK_DGRAM)
+
+server_socket.bind(("", SERVER_PORT))
+
+print("[SERVER] The server is ready to receive messages")
+
+while True:
+    message, client_address = server_socket.recvfrom(MESSAGE_CHUNK_SIZE)
+
+    received_message = message.decode()
+
+    print(
+        f"[SERVER] Received the following message from client {client_address}: {received_message}"
+    )
+
+    server_socket.sendto(received_message.encode(), client_address)
+
+    if received_message == FINISH_SERVER_MESSAGE:
+        print(
+            "[SERVER] The server has received the finish message. Will exit the process."
+        )
+        break
