b7e89d8d2cc6a61be79126c12349758e91489889
Ricardo Morato <rmr@cin.ufpe.br>
2024-06-14 21:45:00 -0300

    feat: broadcast messages to all users

--- server/server.py
+++ server/server.py
@@ -9,7 +9,7 @@ from common.constants import (
 class Server:
     def __init__(self, port: int, host: str = "localhost") -> None:
         self.address = (host, port)
-
+        self.clients = []
         self.socket = socket(AF_INET, SOCK_DGRAM)
 
     def start(self) -> None:
@@ -21,16 +21,31 @@ class Server:
             chunk_message, client_address = self.socket.recvfrom(MESSAGE_CHUNK_SIZE)
 
             received_chunk_message = chunk_message.decode()
+            greeting_message = "hi, meu nome eh "
 
-            if received_chunk_message == FINISH_SERVER_MESSAGE:
-                print(
-                    "[SERVER] The server has received the finish message. Will exit the process."
-                )
+            if received_chunk_message.startswith(greeting_message):
+                client_name = received_chunk_message.split(greeting_message)[1]
 
-                break
+                client = {"address": client_address, "name": client_name}
+
+                self.clients.append(client)
+
+                connected_successfully = "You connected successfully to the chat"
+
+                self.socket.sendto(connected_successfully.encode(), client_address)
+
+                print(f"[SERVER] Client was added to the clients list: {client}")
+                print(f"[SERVER] Current clients list: {self.clients}")
 
             print(
-                f"[SERVER] Received the following chunk from client {client_address}: {received_chunk_message}"
+                f"[SERVER] Received the following chunk from client {client_address}: {received_chunk_message}. "
+                "And will broadcast to the others"
             )
 
-            self.socket.sendto(received_chunk_message.encode(), client_address)
+            print(f"[SERVER] Current clients list: {self.clients}")
+
+            for client in self.clients:
+                if client_address != client["address"]:
+                    self.socket.sendto(
+                        received_chunk_message.encode(), client["address"]
+                    )
