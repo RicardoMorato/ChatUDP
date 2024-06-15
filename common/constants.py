fac52dbe01aa6c6aa505ca1138278f893f9ba9c5
Ricardo Morato <rmr@cin.ufpe.br>
2024-06-14 22:42:04 -0300

    refactor: move constant messages to constants file

--- common/constants.py
+++ common/constants.py
@@ -3,8 +3,14 @@ SERVER_PORT = 12000
 SERVER_HOST = "localhost"
 
 # NOTE: LEMBRAR DE VOLTAR PARA 1024 depois dos testes
-MESSAGE_CHUNK_SIZE = 10
+MESSAGE_CHUNK_SIZE = 100
 
 CLOSE_CLIENT_SOCKET_MESSAGE = "bye"
 
 FINISH_SERVER_MESSAGE = "end_server"
+
+GREETING_MESSAGE = "hi, meu nome eh "
+
+USER_JOINED_THE_ROOM_MESSAGE = " entrou na sala!"
+
+USER_CONNECTED_SUCCESSFULLY_MESSAGE = "Voce entrou no chat com sucesso, seja bem-vindo!"
