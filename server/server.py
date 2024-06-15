7db3d9d06bfd4bb4db4870463f8d8bac8ddf7611
Ricardo Morato <rmr@cin.ufpe.br>
2024-06-14 22:12:32 -0300

    feat: broadcast message sender information to clients

--- server/server.py
+++ server/server.py
@@ -1,7 +1,6 @@
 from socket import *
 
 from common.constants import (
-    FINISH_SERVER_MESSAGE,
     MESSAGE_CHUNK_SIZE,
 )
 
@@ -22,6 +21,7 @@ class Server:
 
             received_chunk_message = chunk_message.decode()
             greeting_message = "hi, meu nome eh "
+            user_joined_the_room_message = " entrou na sala!"
 
             if received_chunk_message.startswith(greeting_message):
                 client_name = received_chunk_message.split(greeting_message)[1]
@@ -32,20 +32,37 @@ class Server:
 
                 connected_successfully = "You connected successfully to the chat"
 
+                # Enviando mensagem "Conectado com sucesso"
                 self.socket.sendto(connected_successfully.encode(), client_address)
+
+                client_joined_message = client_name + user_joined_the_room_message
+
+                # Enviando mensagem "Fulano entrou na sala"
+                for client in self.clients:
+                    if client_address != client["address"]:
+                        self.socket.sendto(
+                            client_joined_message.encode(), client["address"]
+                        )
 
                 print(f"[SERVER] Client was added to the clients list: {client}")
                 print(f"[SERVER] Current clients list: {self.clients}")
+            else:
+                print(
+                    f"[SERVER] Received the following chunk from client {client_address}: {received_chunk_message}. "
+                    "And will broadcast to the others"
+                )
 
-            print(
-                f"[SERVER] Received the following chunk from client {client_address}: {received_chunk_message}. "
-                "And will broadcast to the others"
-            )
+                print(f"[SERVER] Current clients list: {self.clients}")
 
-            print(f"[SERVER] Current clients list: {self.clients}")
+                message_sender = [
+                    c for c in self.clients if c["address"] == client_address
+                ][0]
 
-            for client in self.clients:
-                if client_address != client["address"]:
-                    self.socket.sendto(
-                        received_chunk_message.encode(), client["address"]
-                    )
+                for client in self.clients:
+                    sender_host, sender_port = message_sender["address"]
+                    sender_name = message_sender["name"]
+
+                    message = f"{sender_host}:{sender_port}/~{sender_name}: {received_chunk_message}"
+
+                    if client_address != client["address"]:
+                        self.socket.sendto(message.encode(), client["address"])
