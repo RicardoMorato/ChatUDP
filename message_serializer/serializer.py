cd0d7c9a97ce6334b79d9c91c1de438cb15d1d8e
Ricardo Morato <rmr@cin.ufpe.br>
2024-06-19 18:18:19 -0300

    feat[serializer]: add method to read all content from file

--- message_serializer/serializer.py
+++ message_serializer/serializer.py
@@ -1,5 +1,7 @@
 import os
-from typing import Optional
+
+from typing import Optional, Generator
+
 from common.constants import MESSAGE_CHUNK_SIZE
 
 
@@ -8,7 +10,7 @@ class MessageSerializer:
         self.chunk_size = chunk_size
         self.files_dir = "message_serializer/files/"
 
-    def parse_file_into_message_stream(self, file_path: str = ""):
+    def parse_file_into_message_stream(self, file_path: str = "") -> Generator:
         with open(file_path, "rb") as file:
             while True:
                 content = file.read(self.chunk_size)
@@ -20,7 +22,23 @@ class MessageSerializer:
 
                 yield text
 
-    def build_messages_file(self, file_name: str = "", message: Optional[str] = None):
+    def read_all_content_from_file(self, file_path: str = "") -> str:
+        text = ""
+
+        with open(file_path, "r") as file:
+            while True:
+                content = file.read()
+
+                if not content:
+                    break
+
+                text = text.join(content)
+
+        return text
+
+    def build_messages_file(
+        self, file_name: str = "", message: Optional[str] = None
+    ) -> str:
         file_path = os.path.join(self.files_dir, file_name)
 
         with open(file_path, "a") as file:
@@ -28,7 +46,7 @@ class MessageSerializer:
 
         return file_path
 
-    def remove_file(self, file_name: str = ""):
+    def remove_file(self, file_name: str = "") -> None:
         file_path = os.path.join(self.files_dir, file_name)
 
         if os.path.isfile(file_path):
